9e93fb97043602fe90921a66e7b9766915cfc23d
Alejandro Arraga <alejandroarraga99@gmail.com>
2025-01-14 00:44:31 +0000

    added componet

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Jumbotron = () => {
+    return (
+        <div class="jumbotron jumbotron-fluid text-bg-light p-3">
+            <div class="container">
+                <h1 class="display-4">Fluid jumbotron</h1>
+                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                </p>
+            </div>
+        </div>
+    );
+}
+export default Jumbotron;
